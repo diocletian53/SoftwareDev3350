47ae2830750301e88dc50771e3317e358b93f1b5
diocletian53 <142997289+diocletian53@users.noreply.github.com>
2024-04-24 22:27:05 -0400

    Added Columns
    add columns

--- src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -1,103 +1,130 @@
 package com.luv2code.springboot.thymeleafdemo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate; // Use java.time.LocalDate
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class Employee {
 
-	// define fields
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
-	private int id;
-	
-	@Column(name="first_name")
-	private String firstName;
-	
-	@Column(name="last_name")
-	private String lastName;
-	
-	@Column(name="email")
-	private String email;
-	
-		
-	// define constructors
-	
-	public Employee() {
-		
-	}
-	
-	public Employee(int id, String firstName, String lastName, String email) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
+    // Define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "first_name")
+    private String firstName;
 
-	public Employee(String firstName, String lastName, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
+    @Column(name = "last_name")
+    private String lastName;
 
-	// define getter/setter
-	
-	public int getId() {
-		return id;
-	}
+    @Column(name = "email")
+    private String email;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    @Column(name = "ssn")
+    private String ssn;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    @Column(name = "hire_date")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) // Use the ISO date format
+    private LocalDate hireDate; // Change the type to LocalDate
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    @Column(name = "salary")
+    private BigDecimal salary;
 
-	public String getLastName() {
-		return lastName;
-	}
+    // Define constructors
+    public Employee() {
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public Employee(int id, String firstName, String lastName, String email, String ssn, LocalDate hireDate, BigDecimal salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.ssn = ssn;
+        this.hireDate = hireDate;
+        this.salary = salary;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public Employee(String firstName, String lastName, String email, String ssn, LocalDate hireDate, BigDecimal salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.ssn = ssn;
+        this.hireDate = hireDate;
+        this.salary = salary;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    // Define getter/setter methods
+    public int getId() {
+        return id;
+    }
 
-	// define tostring
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
-	}
-		
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    // Override toString()
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", ssn='" + ssn + '\'' +
+                ", hireDate=" + hireDate +
+                ", salary=" + salary +
+                '}';
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
