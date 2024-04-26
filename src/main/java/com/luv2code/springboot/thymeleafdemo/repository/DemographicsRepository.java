78b78dbe372d230469af6b7d85da92abfee09449
diocletian53 <142997289+diocletian53@users.noreply.github.com>
2024-04-26 17:36:28 -0400

    Revert "Added Demographics table"
    This reverts commit 582d13358e3238a860919f165af06dbc0f177ed6.

--- src/main/java/com/luv2code/springboot/thymeleafdemo/repository/DemographicsRepository.java
+++
@@ -1,9 +0,0 @@
-// DemographicsRepository.java
-package com.luv2code.springboot.thymeleafdemo.repository;
-
-import com.luv2code.springboot.thymeleafdemo.entity.Demographics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DemographicsRepository extends JpaRepository<Demographics, Integer> {
-    // Define custom query methods if needed
-}
