4ba14c0b2eea7d137a154b74d91dc0587fdd644f
diocletian53 <142997289+diocletian53@users.noreply.github.com>
2024-04-28 21:48:40 -0400

    SQL Script Create

--- sql-scripts/employee-directory.sql
+++ sql-scripts/employee-directory.sql
@@ -7,24 +7,11 @@ USE `employee_directory`;
 
 DROP TABLE IF EXISTS `employee`;
 
-CREATE TABLE `employee` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
 -- Data for table `employee`
 --
 
-INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
 
 -- Create the "address" table
 CREATE TABLE address (
@@ -141,6 +128,15 @@ CREATE TABLE payroll (
 
 
 -- Inserts
+INSERT INTO `employee` VALUES 
+	(1,'Leslie','Andrews','leslie@luv2code.com'),
+	(2,'Emma','Baumgarten','emma@luv2code.com'),
+	(3,'Avani','Gupta','avani@luv2code.com'),
+	(4,'Yuri','Petrov','yuri@luv2code.com'),
+	(5,'Juan','Vega','juan@luv2code.com');
+
+
+
 
 -- Populate the "address" table
 INSERT INTO address (empid, addressLine1, addressLine2, cityID, stateID, country, postalCode)
